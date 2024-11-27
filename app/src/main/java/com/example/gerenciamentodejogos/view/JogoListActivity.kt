f06cea6bb517bb5b5672e1abbf86e201b7b89dd2
ZappGod <GABRIELLSZAPP@gmail.com>
2024-11-26 23:40:02 -0300

    colocando a busca

--- app/src/main/java/com/example/gerenciamentodejogos/view/JogoListActivity.kt
+++ app/src/main/java/com/example/gerenciamentodejogos/view/JogoListActivity.kt
@@ -8,10 +8,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,7 +45,27 @@ fun JogoListScreen(viewModel: JogoListViewModel = viewModel(), navController: Na
             Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
                 Text("Lista de Jogos", fontSize = 24.sp)
 
+                // Barra de pesquisa
+                var searchQuery by remember { mutableStateOf("") }
+
+                TextField(
+                    value = searchQuery,
+                    onValueChange = {
+                        searchQuery = it // Atualiza o valor enquanto digita
+                    },
+                    label = { Text("Buscar por título") },
+                    modifier = Modifier.fillMaxWidth(),
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            viewModel.buscarJogosPorTitulo(searchQuery) // Aciona a busca ao clicar na lupa
+                        }) {
+                            Icon(Icons.Default.Search, contentDescription = "Buscar")
+                        }
+                    }
+                )
+
                 Spacer(modifier = Modifier.height(16.dp))
+
 
                 // Exibe a lista de jogos
                 LazyColumn {
